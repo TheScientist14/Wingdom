28c3e50a625df6f8e82f1ac7317b5b901fea9241
TheScientist14 <saurytom@gmail.com>
2021-12-13 02:07:38 +0100

    Improved interaction system

--- Assets/Template/DevTom/Scripts/BrokenTreeBehaviour.cs
+++ Assets/Template/DevTom/Scripts/BrokenTreeBehaviour.cs
@@ -8,68 +8,20 @@ public class BrokenTreeBehaviour : MonoBehaviour
 {
     [SerializeField] MeshRenderer leaves;
     [SerializeField] Material healedMaterial;
-    [SerializeField] Canvas canvas;
-    [SerializeField] TextMeshProUGUI controlHint;
+    [SerializeField] Interactable interactionListener;
 
-    private new Camera camera;
-    private PlayerInput playerInput;
-
-    private bool isHealed = false;
-
-    void Awake()
-    {
-        playerInput = GetComponent<PlayerInput>();
-    }
+    //private bool isHealed = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerInput.enabled = false;
-        camera = Camera.main;
-        canvas.transform.rotation = camera.transform.rotation;
-        controlHint.gameObject.SetActive(false);
+        interactionListener.hasBeenInteracted.AddListener(Heal);
     }
 
-    void OnInteract()
+    void Heal()
     {
         leaves.material = healedMaterial;
-        isHealed = true;
-        SetInteractable(false);
-    }
-
-    /*void OnCameraMove(){
-        controlHint.transform.rotation = camera.transform.rotation;
-        //controlHint.transform.rotation = Quaternion.Euler(camera.transform.rotation.eulerAngles.x, camera.transform.rotation.eulerAngles.y + 180, camera.transform.rotation.eulerAngles.z);
-    }*/
-
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("tree");
-        if (other.gameObject.CompareTag("Player"))
-        {
-            SetInteractable(true);
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            SetInteractable(false);
-        }
-    }
-
-    void SetInteractable(bool isInteractable)
-    {
-        if (!isHealed)
-        {
-            playerInput.enabled = isInteractable;
-            controlHint.gameObject.SetActive(isInteractable);
-        }
-        else
-        {
-            playerInput.enabled = false;
-            controlHint.gameObject.SetActive(false);
-        }
+        //isHealed = true;
+        interactionListener.SetIsInteractable(false);
     }
 }
