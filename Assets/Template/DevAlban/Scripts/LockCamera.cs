31f731a0abdfc826ac66562d07561b6678c9e440
TheScientist14 <saurytom@gmail.com>
2021-12-14 15:50:45 +0100

    Camera able to rotate around player, fixed some camera related player movement issue and ui rotation, imported some assets

--- Assets/Template/DevAlban/Scripts/LockCamera.cs
+++ Assets/Template/DevAlban/Scripts/LockCamera.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LockCamera : MonoBehaviour
 {
-    public Transform player;
-    private Vector3 offset;
+    [SerializeField] Transform player;
+    [SerializeField] float cameraRotationSpeed = 1;
+    //[SerializeField] float smoothFactorCameraRotationSpeed;
+    private float inputValue = 0;
+    //private Vector3 offset;
 
     [Range(0.01f, 1.0f)]
-    public float SmoothFactor = 0.5f;
+    [SerializeField] float smoothFactor = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.position;
+        //offset = transform.position - player.position;
     }
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        Vector3 newPos = player.position + offset;
-        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+        //Vector3 newPos = player.position + offset;
+        transform.position = Vector3.Slerp(transform.position, player.position, smoothFactor);
+        transform.Rotate(0, inputValue * cameraRotationSpeed * Time.fixedDeltaTime, 0, Space.World);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCameraRight(InputValue value)
     {
-        
+        inputValue = value.Get<float>();
     }
-
 }
