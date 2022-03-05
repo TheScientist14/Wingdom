e42f45566bedb3b61fd0d2fb7a410059e7dc09c0
Robin Douet <robin.douet@gmail.com>
2022-03-05 16:42:48 +0100

    Quests following working

--- Assets/Template/Scripts/UI/QuestsMenu.cs
+++ Assets/Template/Scripts/UI/QuestsMenu.cs
@@ -1,12 +1,11 @@
+using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class QuestsMenu : MonoBehaviour
 {
-
     [SerializeField] GameObject questsMenuPanel;
     [SerializeField] GameObject firstSelectedButton;
     [SerializeField] TextMeshProUGUI questName1;
@@ -16,13 +15,13 @@ public class QuestsMenu : MonoBehaviour
     [SerializeField] Button buttonQuest1;
     [SerializeField] Button buttonQuest2;
     [SerializeField] Button buttonQuest3;
+
+    private string _questDescriptionTmp1;
+    private string _questDescriptionTmp2;
+    private string _questDescriptionTmp3;
+
+    private Quest[] _quests;
     
-    private string questDescriptionTmp1;
-    private string questDescriptionTmp2;
-    private string questDescriptionTmp3;
-
-    private Quest[] quests;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -36,13 +35,13 @@ public class QuestsMenu : MonoBehaviour
         buttonQuest3.interactable = false;
 
         questDescription.text = null;
-        questDescriptionTmp1 = null;
-        questDescriptionTmp2 = null;
-        questDescriptionTmp3 = null;
+        _questDescriptionTmp1 = null;
+        _questDescriptionTmp2 = null;
+        _questDescriptionTmp3 = null;
 
-        quests =  FindObjectsOfType<Quest>();
+        _quests = FindObjectsOfType<Quest>();
 
-        foreach (Quest quest in quests)
+        foreach (Quest quest in _quests)
         {
             quest.onQuestStateUpdate.AddListener(UpdateStartedQuest);
         }
@@ -64,161 +63,57 @@ public class QuestsMenu : MonoBehaviour
 
     public void UpdateStartedQuest()
     {
-        /*foreach (Quest quest in quests)
+        questName1.text = null;
+        questName2.text = null;
+        questName3.text = null;
+
+        buttonQuest1.interactable = false;
+        buttonQuest2.interactable = false;
+        buttonQuest3.interactable = false;
+
+        questDescription.text = null;
+        _questDescriptionTmp1 = null;
+        _questDescriptionTmp2 = null;
+        _questDescriptionTmp3 = null;
+
+        foreach (Quest quest in _quests)
         {
             if (quest.GetProgress() == Quest.QuestState.Started)
             {
-                if (questName1.text != null)
+                if (questName1.text == null)
                 {
-                    Debug.Log("questName1 != null");
-                    if (questName2.text == null && questName3.text == null)
-                    {
-                        questName2.text = quest.GetQuestName();
-                        questDescriptionTmp2 = quest.GetQuestDetail();
-                        buttonQuest2.interactable = true;
-                    }
-                    else if (questName2.text != null && questName3.text == null)
-                    {
-                        questName3.text = quest.GetQuestName();
-                        questDescriptionTmp3 = quest.GetQuestDetail();
-                        buttonQuest3.interactable = true;
-                    }
-                    else if (questName2.text == null && questName3.text != null)
-                    {
-                        questName2.text = questName3.text;
-                        questDescriptionTmp2 = questDescriptionTmp3;
-
-                        questName3.text = quest.GetQuestName();
-                        questDescriptionTmp3 = quest.GetQuestDetail();
-
-                        buttonQuest2.interactable = true;
-                        buttonQuest3.interactable = true;
-                    }
+                    questName1.text = quest.GetQuestName();
+                    _questDescriptionTmp1 = quest.GetQuestDetail();
+                    buttonQuest1.interactable = true;
                 }
-                else if (questName1.text == null)
+                else if (questName2.text == null)
                 {
-                    Debug.Log("questName1 == null");
-                    if (questName2.text == null && questName3.text == null)
-                    {
-                        questName1.text = quest.GetQuestName();
-                        questDescriptionTmp1 = quest.GetQuestDetail();
-                        buttonQuest1.interactable = true;
-                    }
-                    else if (questName2.text != null && questName3.text == null)
-                    {
-                        questName1.text = questName2.text;
-                        questDescriptionTmp1 = questDescriptionTmp2;
-
-                        questName2.text = quest.GetQuestName();
-                        questDescriptionTmp2 = quest.GetQuestDetail();
-
-                        buttonQuest1.interactable = true;
-                        buttonQuest2.interactable = true;
-                    }
-                    else if (questName2.text == null && questName3.text != null)
-                    {
-                        questName1.text = questName3.text;
-                        questDescriptionTmp1 = questDescriptionTmp3;
-
-                        questName2.text = quest.GetQuestName();
-                        questDescriptionTmp2 = quest.GetQuestDetail();
-
-                        questName3.text = null;
-                        questDescriptionTmp3 = null;
-
-                        buttonQuest1.interactable = true;
-                        buttonQuest2.interactable = true;
-                        buttonQuest3.interactable = false;
-                    }
-                    else if (questName2.text != null && questName3.text != null)
-                    {
-                        questName1.text = questName2.text;
-                        questDescriptionTmp1 = questDescriptionTmp2;
-
-                        questName2.text = questName3.text;
-                        questDescriptionTmp2 = questDescriptionTmp3;
-
-                        questName3.text = quest.GetQuestName();
-                        questDescriptionTmp3 = quest.GetQuestDetail();
-
-                        buttonQuest1.interactable = true;
-                        buttonQuest2.interactable = true;
-                        buttonQuest3.interactable = true;
-                    }
+                    questName2.text = quest.GetQuestName();
+                    _questDescriptionTmp2 = quest.GetQuestDetail();
+                    buttonQuest2.interactable = true;
+                }
+                else if (questName3.text == null)
+                {
+                    questName3.text = quest.GetQuestName();
+                    _questDescriptionTmp3 = quest.GetQuestDetail();
+                    buttonQuest3.interactable = true;
                 }
             }
-
-            else if (quest.GetProgress() == Quest.QuestState.Completed)
-            {
-                if (quest.GetQuestName() == questName1.text)
-                {
-                    if (questName2.text == null && questName3.text == null)
-                    {
-                        questName1.text = null;
-                        buttonQuest1.interactable = false;
-                    }
-                    else if (questName2.text != null && questName3 == null)
-                    {
-                        questName1.text = questName2.text;
-                        questName2.text = null;
-                        buttonQuest2.interactable = false;
-                    }
-                    else if (questName2.text != null && questName3 != null)
-                    {
-                        questName1.text = questName2.text;
-                        questDescriptionTmp1 = questDescriptionTmp2;
-                        
-                        questName2.text = questName3.text;
-                        questDescriptionTmp2 = questDescriptionTmp3;
-                        
-                        questName3.text = null;
-                        questDescriptionTmp3 = null;
-
-                        buttonQuest3.interactable = false;
-                    }
-                }
-                else if (quest.GetQuestName() == questName2.text)
-                {
-                    if (questName3.text == null)
-                    {
-                        questName2.text = null;
-                        questDescriptionTmp2 = null;
-                        buttonQuest2.interactable = false;
-                    }
-                    else if (questName3 != null)
-                    {
-                        questName2.text = questName3.text;
-                        questDescriptionTmp2 = questDescriptionTmp3;
-                        
-                        questName3.text = null;
-                        questDescriptionTmp3 = null;
-                        
-                        buttonQuest3.interactable = false;
-                    }
-                }
-                else if (quest.GetQuestName() == questName3.text)
-                {
-                    questName3.text = null;
-                    questDescriptionTmp3 = null;
-                    
-                    buttonQuest3.interactable = false;
-                }
-            }
-        }*/
+        }
     }
 
     public void OnClickDetail1()
     {
-        questDescription.text = questDescriptionTmp1;
+        questDescription.text = _questDescriptionTmp1;
     }
-    
+
     public void OnClickDetail2()
     {
-        questDescription.text = questDescriptionTmp2;
+        questDescription.text = _questDescriptionTmp2;
     }
-    
+
     public void OnClickDetail3()
     {
-        questDescription.text = questDescriptionTmp3;
+        questDescription.text = _questDescriptionTmp3;
     }
 }
